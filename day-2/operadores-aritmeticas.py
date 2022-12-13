8ffc16e33786c8659efbc5c89cd1d62a42fb8dc4
Fernando Ortiz <fernandodev.ortiz@gmail.com>
2022-12-12 19:47:24 -0600

    Format strings and learning Math operators

---
+++ day-2/operadores-aritmeticas.py
@@ -0,0 +1,28 @@
+x = 6
+y = 2
+z = 7
+
+print(f"{ x } mas { y } es igual a { x + y }")
+print(f"{ x } menos { y } es igual a { x - y }")
+print(f"{ x } por { y } es igual a { x * y }")
+print(f"{ x } dividido { y } es igual a { x / y }")
+
+print(f"{ z } dividido al piso de { y } es igual a { z//y }")
+print(f"{ z } modulo de { y } es igual a { z % y }")
+print(f"{ x } elevado a la { y } es igual a { x**y }")
+print(f"{ x } elevado a la 3 es igual a { x**3 }")
+print(f"{ x } la raiz cuadra de  { x**0.5 }")
+
+# Ademas de los operadores normales de JS y Python se explican los diferentes operadores aritmeticos:
+"""
+   1. + = Signo más, este tiene la capacidad de sumar valores y concatenar los números o strings.
+   2. - = Signo menos, este tiene la capacida de restar.
+   3. * Por, este signo nos ayuda a obtener el producto de la multiplicacion que se está llevando a cabo.
+   4. Una division al suelo tiene la capacidad de redondear su número, sin embargo, solo lo hace hacia abajo. Por lo que si tengo 2.1. El resultado sería 1 y se eliminaría.
+   5. Módulo de los numeros igual que Javascript.
+   6. Para elevar un número se necesitan dos pares de ** y el número al final que dieron los resultados.
+   7. Para elevar un número al cubo, primero se necesita el valor a elevar o sacar la raiz de cubo, se necesitan dos cosas, los mismo dos asteriscos y por último el valor por el que se quiere calcular.
+   8. Para elevar la raiz cuadrada a la final, debemos de escribir el valor de la variables, dos signos por y el valor el cual queres exportar la comida.
+
+   Si hay alguna duda estoy pendiente.   
+"""
---
+++ day-2/strings-format.py
@@ -0,0 +1,10 @@
+""" x = 10
+y = 5
+
+print("La suma de {} y {} es igual a {}".format(x, y, x + y)) """
+
+color = 'rojo'
+matricula = 541926
+
+print(f"El auto es de color { color } y su matricula es { matricula }")
+# Personalmente prefiero esta ultima forma ya que permite la introduccion de variables de forma explicita en el lugar que queremos que aparezca. Muy igual a los template strings en JS.
