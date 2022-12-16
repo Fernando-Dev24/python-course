1dc15e8bf7a383f841fb76b043fca3ab9ab274ab
Fernando Ortiz <fernandodev.ortiz@gmail.com>
2022-12-15 20:36:37 -0600

    Dia 2 - Teoria, completado

---
+++ day-2/round.py
@@ -0,0 +1,9 @@
+valor = 95.66666
+redondeo = round(valor)
+
+print(redondeo)
+print(type(redondeo))
+
+# Cuando estamos redondeando los valores dentro de la variable estamos cambiando definitivamente su valor, mientras que si lo hacemos afuera, el valor por defecto, o el tipo de dato no cambia puesto que su valor no cambia definitivamente como cuando redondeamos adentro.
+
+# Si no pasamos el numero de digitos decimales, el resultado del redondeo es un numero entero
